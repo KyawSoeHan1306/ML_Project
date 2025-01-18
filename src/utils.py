95440d591ccc336e44134c9e6a6b7286227a7091
Kyaw Soe Han <kyawsoehan1306@gmail.com>
2025-01-18 16:30:49 +0530

    predict_pipeline.py and app.py are added

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 from src.exception import CustomException
 import joblib
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -18,13 +18,18 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report={}
         
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
             
+            gs = GridSearchCV(model,param_grid=para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
             
             y_train_pred = model.predict(X_train)
@@ -37,6 +42,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
             report[list(models.keys())[i]] = test_model_score
             
         return report
-        
     except Exception as e:
             raise CustomException(e, sys)
+        
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return joblib.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
